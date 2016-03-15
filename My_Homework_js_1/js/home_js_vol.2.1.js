9ee17341e5dd979c42e9ee21018efb0183d5e3e8
Viktor Rachok <viktor.rachok@gmail.com>
2016-03-15 22:59:45 +0200

    разрулил решение второго варината для перебора массива без применения булевой переменной

--- My_Homework_js_1/js/home_js_vol.2.1.js
+++ My_Homework_js_1/js/home_js_vol.2.1.js
@@ -1,11 +1,15 @@
+// задаем пременные - булеву, массив, имя для ввода
 var mainArr = [];
 var i, name, myBool = false;
 
+// наполняем массив именами
 for (i = 0; i < 5; i++) {
   mainArr[i] = prompt('Введите имя'+ (i + 1),'');
   console.log(mainArr[i]);
 }
 console.log(mainArr);
+
+// задаем условие проверки имени на совпадение с данными в массиве
 var passwordUser = 'qwerty';
 var yourName = prompt('Введите имя для проверки','');
 
@@ -15,9 +19,12 @@ for (i = 0; i < 5; i++) {
     break;
   }
 }
+
+// на основании полученного значения булевой переменной
+// выводим необходимое нам сообщение
 if (myBool){
     alert('Добро пожаловать '+ yourName);
-    var pass = prompt('введите Ваш пароль');
+    var pass = prompt('введите Ваш пароль','');
     if (pass == passwordUser) {
       alert('Вы прошли индентификацию!');
     } else { alert('пароль неверный');
