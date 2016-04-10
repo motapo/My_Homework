1b15923d5032eae69580cfca3dee2d60a11d4cd9
Viktor Rachok <viktor.rachok@gmail.com>
2016-04-10 23:49:56 +0300

    добавил грустного Гранта...

--- My_Homework_js_18_Gulp/gulpfile.js
+++ My_Homework_js_18_Gulp/gulpfile.js
@@ -4,6 +4,13 @@ var cssmin = require('gulp-cssmin');
 var uglify = require('gulp-uglify');
 //watch есть по умолчанию, поэтому его можно не прописывать как переменную
 //var watch = require('gulp-watch');
+var plumber = require('gulp-plumber'); //отлавливает ошибки кода
+// для его работы в таск с ошибками добавляется строка
+// .pipe(plumber())
+
+//если прописать в дефолтном таске все нужные команды, 
+//то можно вызывать их выполнение командой gulp
+//gulp.task('default', ['cssMin', 'jsUglify', 'watch']);
 
 gulp.task('watch', function(){
 	gulp.watch('./css/**/*.css', ['cssMin']);
@@ -12,7 +19,7 @@ gulp.task('watch', function(){
 //объединяем и минимизируем файлы JS
 gulp.task('jsUglify', function(){
 	//объединяем и минимизируем файлы JS самописанные
-	gulp.src('./js/home_work_js_9_10_*.js')
+	gulp.src(['./js/home_work_js_9_10_*.js', './js/grunt_sad.js'])
 	.pipe(uglify())
 	.pipe(concat('all.min.js'))
 	.pipe(gulp.dest('./dist'));
@@ -37,8 +44,9 @@ gulp.task('cssMin', function(){
 	//снова важен порядок, поэтому делаем массив
 	gulp.src(['css/reset.css', 'css/style_jcarousel.css',
 		'css/style_s*.css', 'css/style_menu.css',
-		'css/style_ch*.css'
+		'css/style_ch*.css', 'css/fff_tusjbold.css'
 		 ])
+	// .pipe(plumber()) // отлавливает ошибки кода
 	.pipe(cssmin())
 	.pipe(concat('all.min.css'))
 	.pipe(gulp.dest('./dist'));
