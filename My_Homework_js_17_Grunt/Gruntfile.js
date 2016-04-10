5ecfd63eee5c63e1df07ccf57e4b8a73ff67bb34
Viktor Rachok <viktor.rachok@gmail.com>
2016-04-10 09:04:49 +0300

    дожал min.css

---
+++ My_Homework_js_17_Grunt/Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: [
+                    'js/libs/*.js',  
+                    'js/home_work_js_9_10_checkbox.js',  
+                    'js/home_work_js_9_10_jcarousel.js',  
+                    'js/home_work_js_9_10_menu.js',  
+                    'js/home_work_js_9_10_select.js'  
+                ],
+                dest: 'js/build/production.js'
+            },
+            dist: {
+                src: 'css/*.css', 
+                dest: 'css/build/production.css'
+            }
+        },    
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }    
+        },
+        cssmin: {
+            build: {
+                src: 'css/build/production.css',
+                dest: 'css/build/production.min.css'
+            }
+        },        
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'img/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'img/build/'
+                }]
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                spawn: false,
+                },
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'imagemin']);
+};
---
+++ My_Homework_js_17_Grunt/js/home_work_js_9_10_menu.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $('a').on('click', function(e){
+    e.preventDefault();
+  });
+    
+  $('.name_li').hover(function () {
+     clearTimeout($.data(this,'timer'));
+     $('.name_brend',this).stop(true,true).slideDown(200);
+  }, function () {
+    $.data(this,'timer', setTimeout($.proxy(function() {
+      $('.name_brend',this).stop(true,true).slideUp(200);
+    }, this), 100));
+  });
+
+  $('.name_brend_sub').hover(function () {
+     clearTimeout($.data(this,'timer'));
+     $('.main_menu_sub',this).stop(true,true).slideDown(200);
+  }, function () {
+    $.data(this,'timer', setTimeout($.proxy(function() {
+      $('.main_menu_sub',this).stop(true,true).slideUp(200);
+    }, this), 100));
+  });
+   
+  $('.main_menu_sub_item').hover(function () {
+     clearTimeout($.data(this,'timer'));
+     $('.main_menu_sub_item_ul',this).stop(true,true).slideDown(200);
+  }, function () {
+    $.data(this,'timer', setTimeout($.proxy(function() {
+      $('.main_menu_sub_item_ul',this).stop(true,true).slideUp(200);
+    }, this), 100));
+  });
+});
+
+
